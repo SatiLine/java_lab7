342e8a51d46a6447d329f781faa63b492dd9d171
Sati_Line <117307966+SatiLine@users.noreply.github.com>
2024-12-18 02:56:39 +0700

    Update EvenNumbers.java

--- app/src/main/java/org/example/taskone/EvenNumbers.java
+++ app/src/main/java/org/example/taskone/EvenNumbers.java
@@ -2,14 +2,17 @@ package org.example.taskone;
 
 import java.util.Random;
 
-public final class EvenNumbers extends java.lang.Thread {
+public final class EvenNumbers extends java.lang.Thread { //наследует класс Thread,
+                                                          //что позволяет ему выполнять код в отдельном потоке.
     @Override
-    public void run() {
-        new Random()
-            .ints(-1000, 1001)
-            .filter(number -> number % 2 == 0)
-            .limit(5)
-            .forEach(System.out::println);
+    public void run() { // Метод run() переопределяется из класса Thread.
+                        //Этот метод содержит код, который будет выполняться в новом потоке. 
+        new Random() // создаем новый экземпляр класса
+            .ints(-1000, 1001) // метод генерирует бесконечный поток случайных целых чисел
+            .filter(number -> number % 2 == 0) // оставляет только четные числа
+            .limit(5) 
+            .forEach(System.out::println); // метод применяется к оставшимся элементам потока 
+                                           //и выводит каждое четное число на консоль
     }
 }
 
