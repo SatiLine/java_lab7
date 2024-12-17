f040c08e2efba2bbd6b2085bc3eebe8a26abce80
Sati_Line <117307966+SatiLine@users.noreply.github.com>
2024-12-18 03:24:43 +0700

    Update Order.java

--- app/src/main/java/org/example/tasktwo/Order.java
+++ app/src/main/java/org/example/tasktwo/Order.java
@@ -1,6 +1,8 @@
 package org.example.tasktwo;
 
-public final record Order(int id, ShoeType type, int quantity) {
+public final record Order(int id, ShoeType type, int quantity) { //records -- специальный тип класса, 
+                                                                 //который автоматически предоставляет реализацию 
+                                                                 //методов equals(), hashCode(), и toString() для своих полей.
     public Order {
         if(id < 0) {
             throw new IllegalArgumentException("Order ID should be a non-negative number.");
