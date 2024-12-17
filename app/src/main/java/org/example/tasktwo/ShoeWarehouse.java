c59feee29fc80bc6d53ef5b9cc8487e85409b240
Sati_Line <117307966+SatiLine@users.noreply.github.com>
2024-12-18 04:07:43 +0700

    Update ShoeWarehouse.java

--- app/src/main/java/org/example/tasktwo/ShoeWarehouse.java
+++ app/src/main/java/org/example/tasktwo/ShoeWarehouse.java
@@ -1,18 +1,18 @@
 package org.example.tasktwo;
 
 import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.Arrays;
+import java.util.concurrent.BlockingQueue; //интерфейс для очередей, поддерживающих блокировку при добавлении и удалении элементов.
+import java.util.concurrent.LinkedBlockingQueue; //реализация BlockingQueue, которая использует связный список для хранения элементов.
+import java.util.Arrays; //для работы с массивами, в данном случае для преобразования массива типов обуви в список.
 
 public final class ShoeWarehouse {
     public final static List<ShoeType> shoeTypes = Arrays.asList(ShoeType.values());
     public final static int MAX_ORDERS = 5;
-    private final BlockingQueue<Order> orders = new LinkedBlockingQueue<Order>(MAX_ORDERS);
+    private final BlockingQueue<Order> orders = new LinkedBlockingQueue<Order>(MAX_ORDERS); //очередь для управления заказами
 
     public void receiveOrder(final Order order) {
         try {
-            orders.put(order);
+            orders.put(order); //Метод put() добавляет заказ в очередь
             System.out.println("New order: " + order);
         }
         catch (final Exception e) {
@@ -22,7 +22,7 @@ public final class ShoeWarehouse {
 
     public Order fulfillOrder() {
         try {
-            final Order order = orders.take();
+            final Order order = orders.take(); //Метод take() удаляет и возвращает следующий заказ из очереди
             System.out.println("Order processed: " + order);
             return order;
         }
